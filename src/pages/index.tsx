6ae724a2d49a882a9091b0791f965aa94e1aef8e
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-05-24 19:48:09 +0100

    chore: Landing page

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const Home: React.FC = () => {
+  return (
+    <Box>
+      <Heading>Welcome to chakra UI</Heading>
+    </Box>
+  );
+};
+
+export default Home;
